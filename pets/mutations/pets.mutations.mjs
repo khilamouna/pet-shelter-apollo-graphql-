badb1e472e4e6fde9c1b6d314a54e4216e3ae0b1
mouna <mouna.khila@aleriaconsulting.eu>
2023-08-30 16:29:04 +0100

    automatic test implementation

--- pets/mutations/pets.mutations.mjs
+++ pets/mutations/pets.mutations.mjs
@@ -2,25 +2,26 @@ import { editItem, addItem } from "../models/pets.models.mjs";
 export const addPet = (petToAdd) => {
   try {
     const resp = addItem(petToAdd);
-
     return resp;
-  } catch (error) {
-    return error;
+  } catch (err) {
+    return err;
   }
 };
+
 export const editPet = (petToEdit) => {
   try {
     const resp = editItem(petToEdit?.id, petToEdit);
     return resp;
-  } catch (error) {
-    return error.message;
+  } catch (err) {
+    return err;
   }
 };
+
 export const deletePet = (id) => {
   try {
     const resp = deleteItem(id);
     return resp;
-  } catch (error) {
-    return error;
+  } catch (err) {
+    return err;
   }
 };
